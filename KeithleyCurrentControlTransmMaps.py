40fda0895b924f218cc61354f11fbfec684b3508
Nathalia Tomazio <nathalia.tomazio@alumni.usp.br>
2021-12-12 20:04:26 -0300

    code was updated to be more organized

--- KeithleyCurrentControlTransmMaps.py
+++ KeithleyCurrentControlTransmMaps.py
@@ -15,6 +15,7 @@ import controller # NI DAQ
 import pickle
 import keithley2400 # current controller for the heaters
 import time
+import sys
 
 # --- define the objects for the laser, oscilloscope, and current controller --- #
 laser = agilent816xb.Agilent816xb()  # laser 
@@ -71,21 +72,72 @@ task.add_channel("Dev1/ai3")    # mzi
 
 # --- Config for the current controller (keithley) ---#
 keithley.connectSM()
-
+keithley.conf_apply_current()
 # change this
-Nmean = 10 # number of measurements taken to compute the averaged resistence 
+keithley.set_compliance_voltage(10) # initialize the compliance voltage [Volts]
+
+# --- Define important functions ---#
+
+# --- Define function to measure the resistence ---#
+def measResistence(curr, wait=2):
+    """
+    Measure resistence given a current level
+        input:
+            curr: current [mA] 
+            wait: time for the temperature to stabilize [seconds] 
+        output:
+            resistence: [Ohm]
+            dissipated power: [mW]
+    """
+    # initialize variables
+    cond = 1
+    tol = 0.01
+    R = 0
+    stdR = 0
+    Power = 0
+
+    # define the number of measurements taken to compute the averaged resistence
+    Nmean = 10 
+
+    keithley.set_source_current(curr*1e-3) # set a current in A
+    time.sleep(wait) # wait for the temperature to stabilize 
+
+    while cond > tol:
+        # take the averaged resistence value      
+        res = []
+        for ii in range(Nmean):
+            res.append(keithley.get_ohms()) # get the resistence in Ohm
+
+        if abs(np.mean(res)) < 1e3: # if the resistence is smaller than 1k Ohm -> True
+            cond = np.std(res)/np.mean(res)
+            if cond < tol:
+                R = np.mean(res)
+                stdR = np.std(res)
+                Power = (curr*1e-3)**2/np.mean(res)*1e3 # in mW
+            else:
+                print(f"resistence fluctuation measured @ {curr} mA is beyond toleration")
+        else:
+            print("no electrical contact - make sure the probes are touching the pads")
+            sys.exit() # stop the code
+    
+    print(f"resistence @ {curr} mA is {R:.2f} +/- {stdR:.2f} Ohm")
+    return R, Power
 
 
-# --- Check whether the time delay is enough to acquire the resistence ---#
-CheckTime = False
-if CheckTime:
-    keithley.conf_apply_current()
-    keithley.set_compliance_voltage(10) # in Volts
-     
-    I = 10*1e-3 # provide the current in A
+# --- define function to check whether the time delay is enough to acquire the resistence ---#
+def CheckTime(curr=10,maxtime=20):
+    """
+    Check if the time to wait before resistence acquisition is enough
+        input:
+            curr: current [mA] 
+            maxtime: max amount of time to be tested [seconds] 
+        output:
+           plot of the resistence vs wait time
+    """
+    I = curr*1e-3
 
     R = []
-    wait = [i for i in range(21)]
+    wait = [i for i in range(maxtime+1)]
     for t in wait:
         keithley.set_source_current(I)
         time.sleep(t)
@@ -105,167 +157,86 @@ if CheckTime:
     plt.ylabel("Resistence (Ohm)")
     plt.show()
 
-# change these
-wait = 2 # in seconds, time delay for the temperature to stabilize 
 
-# change these
-maxCurr = 25 # in mA
-step = 2 # in mA
-curr = [i*1e-3 for i in range(0,maxCurr+step,step)] # current list in A
+# --- define function to plot power vs current ---#
+def plotPwr(maxCurr, step, plot=True):
+    """
+    Plot power, resistence vs current curves for checking purposes
+        input:
+            maxCurr: max current for the plot [mA] 
+            step: current increment for the plot [mA] 
+        output:
+           plot of the power, resistence vs current
+    """
+    current = [i for i in range(0,maxCurr+step,step)] # current list in mA
 
+    # initialize variables
+    R = []
+    stdR = []
+    Power = []
 
-# --- Set the compliance voltage - max voltage the source can apply to meet the current requested by the user ---#
-keithley.conf_apply_current()
-keithley.set_compliance_voltage(10) # in Volts 
-keithley.set_source_current(maxCurr*1e-3) 
-
-time.sleep(wait) # wait for the temperature to stabilize 
-
-# take the averaged resistence value 
-maxRes = []
-for ii in range(Nmean):
-    maxRes.append(keithley.get_ohms()) # get the resistence in Ohms 
-R = np.mean(maxRes)
-stdR = np.std(maxRes)
-print(f"resistence @ {maxCurr} mA (max current) =  {round(R,3)} +/- {round(stdR,3)} Ohms")
-
-if R < 1e3: # if the resistence is smaller than 1k Ohm
-    if stdR/R < 0.01:
-        maxVolt = R*maxCurr*1e-3
-        keithley.set_compliance_voltage(np.ceil(maxVolt)+1) # in Volts 
-        print(f"cmpl voltage is set to {np.ceil(maxVolt)+1} V")
-    else:
-        print("resistence fluctuation is beyond toleration")
-else: 
-    print("no electrical contact - make sure the probes are touching the pads")
-
-
-# --- Obtain the resistence and dissipated power as a function of current to ultimately built a linearly spaced power vector ---#
-R = []
-stdR = []
-Power = []
-
-curve = False 
-if curve:
-    for i in curr: 
-        keithley.set_source_current(i) # in A
-        if i == 0:
-            R = [0]
-            stdR = [0]
-            Power = [0]
-        else:
-            time.sleep(wait) # wait for the temperature to stabilize 
-
-            # take the averaged resistence value     
-            res = []
-            for ii in range(Nmean):
-                res.append(keithley.get_ohms())
-
-            if np.mean(res) < 1e3:
-                if np.std(res)/np.mean(res) < 0.01:
-                    R.append(np.mean(res))
-                    stdR.append(np.std(res))
-                    Power.append(i**2/np.mean(res)*1e3) # in mW
-                else:
-                    print(f"resistence fluctuation measured @ {i} A is beyond toleration")
-            else:
-                print("no electrical contact - make sure the probes are touching the pads")
-
+    for curr in current:
+        res, pwr = measResistence(curr)
+        R.append(res)
+        stdR.append(res)
+        Power.append((curr*1e-3)**2/res*1e3) # in mW
+    
     keithley.set_source_current(0) # in A
 
-    plot = True
     if plot:
         fig, ax = plt.subplots(2, sharex=True)
-        ax[0].plot(np.array(curr)*1e3, R,'-o')
-        ax[1].plot(np.array(curr)*1e3, Power, '-o')
+        ax[0].plot(current, R,'-o')
+        ax[1].plot(current, Power, '-o')
         ax[1].set_xlabel("current (mA)")
         ax[0].set_ylabel("Resistence (Ohm)")
         ax[1].set_ylabel("Power (mW)")
         fig.tight_layout()
         plt.show()
 
-    MaxPower = Power[len(Power)-1] # in mW
-    resGuess = np.mean(R[1::]) # resistence guess to calculate the current needed to meet the intended power
 
-else: 
-    keithley.set_source_current(maxCurr*1e-3) 
-    time.sleep(wait) # wait for the temperature to stabilize 
+# --- Set the compliance voltage - max voltage the source can apply to meet the current requested by the user ---#
+# change these
+maxCurr = 25 # in mA
 
-    # take the averaged resistence value     
-    res = []
-    for ii in range(Nmean):
-        res.append(keithley.get_ohms())
-
-    if np.mean(res) < 1e3:
-        if np.std(res)/np.mean(res) < 0.01:
-            R = np.mean(res)
-            stdR = np.std(res)
-            MaxPower = (maxCurr*1e-3)**2/np.mean(res)*1e3 # in mW
-        else:
-            print(f"resistence fluctuation measured @ {maxCurr} mA is beyond toleration")
-    else:
-        print("no electrical contact - make sure the probes are touching the pads")
-
-    keithley.set_source_current(0) # in A
-    resGuess = R # resistence guess to calculate the current needed to meet the intended power
+R, MaxPower = measResistence(maxCurr)
+maxVolt = R*maxCurr*1e-3 # in Volts 
+keithley.set_compliance_voltage(np.ceil(maxVolt)+1) 
+print(f"cmpl voltage is set to {np.ceil(maxVolt)+1} V")
+keithley.set_source_current(0) # in A
 
 
+# --- built a linearly spaced power vector ---#
+# change these
 step = 20
-PotLinear = np.linspace(0,round(MaxPower,3), step) # linearly spaced vector with dissipated power levels in mW
+# PotLinear = np.linspace(0,round(MaxPower,3), step) # linearly spaced vector with dissipated power levels in mW
+PotLinear = np.linspace(0, MaxPower, step)
 
 
 # --- Obtain a current vector to meet the linearly spaced dissipated power vector and save the spectra ---#
-resistence = [0]
-current = [0]
-Power = 0
+
+# initialize variables
+resGuess = R # resistence guess to calculate the current needed to meet the intended power
+resistence = []
+current = []
 i = 0
-for p in PotLinear[1::]:
-    while np.abs(p-Power)/p > 0.01:  
-        curr = np.sqrt(p*1e-3*resGuess) # calculate current in A 
-        keithley.set_source_current(curr) # set current in A
-        time.sleep(wait) # wait for the temperature to stabilize 
+wait = 2
 
-        # take the averaged resistence value     
-        res = []
-        for ii in range(Nmean):
-            res.append(keithley.get_ohms())
+# scan the power levels 
+for p in PotLinear[0:5]:
+    if p == 0:
+        R, curr, Power = (0,0,0) # take the averaged resistence value 
+    else:
+        while np.abs(p-Power)/p > 0.01:  
+            curr = np.sqrt(p*1e-3*resGuess)*1e3 # calculate current in mA 
+            R, Power = measResistence(curr) # take the averaged resistence value 
 
-        if np.mean(res) < 1e3:
-            if np.std(res)/np.mean(res) < 0.01:
-                R = np.mean(res)
-                stdR = np.std(res)
-                Power = curr**2/np.mean(res)*1e3 # in mW
-            else:
-                print(f"resistence fluctuation measured @ {curr} mA is beyond toleration")
-        else:
-            print("no electrical contact - make sure the probes are touching the pads")
+            resGuess = R # update the resistence guess
+            print(f"for iteraction # {i}, calculated power is off the target by {np.abs(p-Power)/p}")
 
-        resGuess = R # update the resistence guess
-        print(f"for iteraction # {i}, calculated power is off the target by {np.abs(p-Power)/p}")
+    resistence.append(R)
+    current.append(curr) 
 
-    resistence.append(resGuess)
-    current.append(np.sqrt(p*1e-3*resGuess)) # calculate current in A 
-
-    i = i+1
-
-
-#--- Save the heater power and current to a cvs file ---#
-df0 = pd.DataFrame({'heater power (mW)': PotLinear, 'resistence (Ohm)': resistence, 'current (mA)': np.array(current)*1e3})
-
-save = True
-if save: 
-    filedir = 'C:\\Users\\Lab\\Documents\\Nathalia Tomazio\\python codes\\transmission data\\C-band_heater control\\'
-    #change this
-    filename = f'08DEZ21_chip2_R-2R-R_wg-ring gap 800 nm_ring-ring gap 500 nm_TM_heater1'
-    filepath = filedir + filename
-    df0.to_csv(filepath+'.csv')
-
-
-
-#--- Scan the current vector and save the spectra ---#
-i = 0
-for curr in current:
-    keithley.set_source_current(curr) # set current in A
+    keithley.set_source_current(curr*1e-3) # set current in A
     time.sleep(wait) # wait for the temperature to stabilize
     
     laser.setSweepState(0,"Start") # enable the laser sweep
@@ -297,14 +268,82 @@ for curr in current:
 
     # change these
     filedir = 'C:\\Users\\Lab\\Documents\\Nathalia Tomazio\\python codes\\transmission data\\C-band_heater control\\'
-    filename = f'08DEZ21_chip2_R-2R-R_wg-ring gap 800 nm_ring-ring gap 500 nm_TM_heater1_{curr*1e3:.2f}mA_{PotLinear[i]:.2e}mW_{i}'
-    filepath = filedir + filename
+    filename = 'test_10DEZ21_chip2_R-2R-R_wg-ring gap 600 nm_ring-ring gap 500 nm_TE_heater1_'
+    if i<10:
+        compl = f'0{i}_{curr*1e3:.2f}mA_{p:.2e}mW'
+    else:
+        compl = f'{i}_{curr*1e3:.2f}mA_{p:.2e}mW'
+    filepath = filedir + filename + compl
     print('saved data: '+filepath+'.pkl')
     # df.to_parquet(filepath+'.parq', compression='brotli')
     picklefile = open(f"{filepath}.pkl", 'wb')
     pickle.dump(df, picklefile)
+
     picklefile.close()
 
     i = i+1
+
+
+#--- Save the heater power and current to a cvs file ---#
+df0 = pd.DataFrame({'heater power (mW)': PotLinear[0:5], 'resistence (Ohm)': resistence, 'current (mA)': current})
+
+save = True
+if save: 
+    filedir = 'C:\\Users\\Lab\\Documents\\Nathalia Tomazio\\python codes\\transmission data\\C-band_heater control\\'
+    #change this
+    filename = f'test_08DEZ21_chip2_R-2R-R_wg-ring gap 600 nm_ring-ring gap 500 nm_TE_heater1'
+    filepath = filedir + filename
+    df0.to_csv(filepath+'.csv')
+
+
+
+# #--- Scan the current vector and save the spectra ---#
+# i = 0
+# for curr in current:
+#     keithley.set_source_current(curr) # set current in A
+#     time.sleep(wait) # wait for the temperature to stabilize
+    
+#     laser.setSweepState(0,"Start") # enable the laser sweep
+
+#     # save spectrum 
+#     task.acquisition_mode = 'N Samples'
+#     task.rate = sampRate
+#     task.samples = int(sampRate*(scan_time+2))
+#     task.timeout = 1.5*scan_time
+#     print(f"Sampling rate: {sampRate} Pts/s")
+#     print(f"Trace length: {int(sampRate*(scan_time+2))}")
+#     task.config()
+#     r = task.read()
+#     task.close()
+
+#     laser.setSweepState(0,"Stop") # enable the laser sweep
+
+#     plot = False
+#     if plot:
+#         plt.plot(-0.25+r[0,:]/20, label = "laser trigger")
+#         plt.plot(0.9+r[1,:],label = "cav")
+#         plt.plot(0.5+r[2,:], label = "acetylene")
+#         plt.plot(r[3,:]/10, label = "MZI")
+#         # plt.legend()
+#         plt.show()
+
+#     df = pd.DataFrame({'trigger_laser': r[0,:], 'cav': r[1,:], 'acetylene': r[2,:], 'mzi': r[3,:]})
+#     # df.hvplot.line(y=['cav','acetylene','mzi'], width=1000, height=300, datashade=True, hover=False))
+
+#     # change these
+#     filedir = 'C:\\Users\\Lab\\Documents\\Nathalia Tomazio\\python codes\\transmission data\\C-band_heater control\\'
+#     if i<10:
+#         filename = f'08DEZ21_chip2_R-2R-R_wg-ring gap 800 nm_ring-ring gap 500 nm_TM_heater1_0{i}_{curr*1e3:.2f}mA_{PotLinear[i]:.2e}mW'
+#     else:
+#         filename = f'08DEZ21_chip2_R-2R-R_wg-ring gap 800 nm_ring-ring gap 500 nm_TM_heater1_{i}_{curr*1e3:.2f}mA_{PotLinear[i]:.2e}mW'
+#     filepath = filedir + filename
+#     print('saved data: '+filepath+'.pkl')
+#     # df.to_parquet(filepath+'.parq', compression='brotli')
+#     picklefile = open(f"{filepath}.pkl", 'wb')
+#     pickle.dump(df, picklefile)
+
+#     picklefile.close()
+
+#     i = i+1
     
 keithley.set_source_current(0) # set current in A
